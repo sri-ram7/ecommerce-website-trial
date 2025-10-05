873e8bbf3bae1fcf9046a58657881d81fc43f6fe
Sriram <tsriram807@gmail.com>
2025-10-05 17:40:20 +0530

    Update offers.jsx

--- frontend/src/components/Offers/offers.jsx
+++ frontend/src/components/Offers/offers.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './offer.css';
-import exclusive_image from '../assests/exclusive_image.png';
+import exclusive_image from '../assests/picture-elegant-young-fashion-man.jpg';
 const Offers= () => {  
-    return (
+     return (
         <div className='offers'>
-           <div className="offers-left">
-               <h1>Exclusive</h1>
-               <h1>Offers For You</h1>
-               <p>ONLY ON BEST SELLERS PRODUCTS</p>
-               <button>Check Now</button>
-           </div>
-           <div className="offe-right">
-                <img src={exclusive_image} alt="exclusive-image" />
-           </div>
-
+            <div className='offer-box-left '></div> 
+            <div className="offers-box">
+                <div className="offers-left autoshow">
+                    <h1>Exclusive</h1>
+                    <h1>Offers For You</h1>
+                    <p>ONLY ON BEST SELLERS PRODUCTS</p>
+                    <button>Check Now</button>
+                </div>
+                <div className="offe-right">
+                    <img src={exclusive_image} alt="exclusive-image" />
+                </div>
+            </div>
+            <div className='offer-box-right'></div> 
         </div>
     );
 }
