3ce299aaee5e005f50d58f49339d4c819452c9ec
Sriram <tsriram807@gmail.com>
2025-10-05 22:16:12 +0530

    Update ProductDisplay.jsx

--- frontend/src/components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/components/ProductDisplay/ProductDisplay.jsx
@@ -11,10 +11,10 @@ const ProductDisplay = ({ product }) => {
     <div className="productdisplay">
       <div className="productdisplay-left">
         <div className="productdisplay-img-list">
-          <img src={product.image} alt={product.name} />
-          <img src={product.image} alt={product.name} />
-          <img src={product.image} alt={product.name} />
-          <img src={product.image} alt={product.name} />
+          <img src={product.image} className='photos' alt={product.name} />
+          <img src={product.image} className='photos' alt={product.name} />
+          <img src={product.image} className='photos' alt={product.name} />
+          <img src={product.image} className='photos' alt={product.name} />
         </div>
         <div className="productdisplay-img">
           <img className='productdisplay-main-img' src={product.image} alt={product.name} />
@@ -35,11 +35,11 @@ const ProductDisplay = ({ product }) => {
           <div className="productdisplay-right-price-new">${product.new_price}</div>
         </div>
         <div className="productdisplay-right-description">
-          A lightweight, usually knitted, pullover.
+          This elegant dress is designed for effortless style and comfort. Made from soft, breathable fabric, it drapes beautifully on all body types and offers a flattering silhouette. Perfect for both casual outings and special occasions, it features subtle detailing, with a comfortable fit that allows you to move freely. Available in multiple colors, this versatile dress will be a staple piece in your wardrobe for seasons to come.
         </div>
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
-          <div className="productdisplay-right-size">
+          <div className="productdisplay-right-sizes">
             <div>S</div>
             <div>M</div>
             <div>L</div>
