b1ea62c4e433368e3d25d4d1b86219b72fcaff3c
Sriram <tsriram807@gmail.com>
2025-10-05 15:31:03 +0530

    Update Popular.jsx

--- frontend/src/components/Popular/Popular.jsx
+++ frontend/src/components/Popular/Popular.jsx
@@ -6,11 +6,11 @@ import Item from '../item/item.jsx';
 const  Popular = () => {
     return (
         <div className='popular'>
-            <h1>Popular in women</h1>
+            <h1 className='autoshow'>Popular in women</h1>
             <hr/ >
-            <div className='popular-item'>
+            <div className='popular-item imagereveal'>
                 {data_product.map((item,i) =>{
-                    return <Item key = {i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+                    return <Item key = {i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} style={{ color: 'white' }}/>
                 })}
             </div>
         </div>
