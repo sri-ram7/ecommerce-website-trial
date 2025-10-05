80164898fc8e9a85615d693077f697b2938f29c2
Sriram <tsriram807@gmail.com>
2025-10-05 19:33:51 +0530

    Update NewsLetter.jsx

--- frontend/src/components/NewsLetter/NewsLetter.jsx
+++ frontend/src/components/NewsLetter/NewsLetter.jsx
@@ -3,14 +3,17 @@ import './NewsLetter.css';
 
 const NewsLetter = () => {  
     return (
-      <div className='newsletter'>
+      <div className="box"> 
+        <div className='newsletter autoshow'>
           <h1>Exclusive Offers On Your Email</h1>
           <p>Subscribe to our newsletter and stay updated</p>
           <div>
             <input type = "email" placeholder='Enter your email' />
             <button> Subscribe </button>
           </div>
+        </div>
       </div>
+      
     )
 }
 
